e0b7d0b7f3be3efdf884f91a8dee8dad3d0f3fa9
felix@deepin <huxiaofei@unisound.com>
2018-06-11 15:37:02 +0800

    添加pm2

--- post-build.js
+++ post-build.js
@@ -5,7 +5,23 @@
 'use strict';
 const fs   = require('fs');
 const path = require('path')
-let pkg    = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf-8'))
-delete pkg.scripts
+const pm2  = require('./ecosystem.config.js')
+
+//create package.json
+let pkg     = JSON.parse(fs.readFileSync(path.resolve('package.json'), 'utf-8'))
+pkg.scripts = {
+  pm2: pkg.scripts.pm2
+}
 delete pkg.devDependencies
 fs.writeFileSync(path.resolve('dist', 'package.json'), JSON.stringify(pkg))
+
+// create pm2 config
+const app = pm2.apps[0]
+app.name  = pkg.name
+fs.readdirSync(path.resolve('dist')).forEach(file => {
+  if (file.includes('main.bundle')) {
+    app.script = file
+  }
+})
+
+fs.writeFileSync(path.resolve('dist', 'ecosystem.config.js'), `module.exports=${JSON.stringify(pm2)}`)
